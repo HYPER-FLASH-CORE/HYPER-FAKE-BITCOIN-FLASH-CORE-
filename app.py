cf866fd5c90490b0370f8cddc26c85f9b354bfeb
HYPER-FLASH-CORE <hackerxy.united1@gmail.com>
2024-10-26 23:53:11 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+def open_link(url):
+    webbrowser.open(url)
+
+if __name__ == "__main__":
+    url = "https://hack-net-noir-softwares.github.io/USDT-CORE-"  # Put your desired URL here
+    open_link(url)
